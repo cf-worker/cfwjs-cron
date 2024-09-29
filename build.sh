c7e332aaa393231bdefa39d363b83124dafceae7
Marcos Neves <neves64@icloud.com>
2024-09-29 17:39:18 -0300

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # run only if in CI
-"$WORKERS_CI" != "1" && exit 0
+[ "$WORKERS_CI" != "1" ] && exit 0
 # append build time to index.js
 bun --print '`BUILT_AT = "${new Date().toJSON()}"`' >> index.js
